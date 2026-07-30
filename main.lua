9c8767cc99f0277d00f3b92bd6577f3684a57e1c
BACONHACKS475 <alekseylys96@gmail.com>
2026-07-30 22:08:08 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BACONHACKS475/BaconPanel/main/main.lua"))()ScreenGui.Parent = game:GetService("CoreGui")
+ScreenGui.Parent = game:GetService("CoreGui") KO
